dd7669014ddab7cb83ef51a92625d20dac9c4923
BugHunter-X <askzenai@gmail.com>
2025-01-20 23:00:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+```javascript
+// Solution: Adjusting class order or resolving CSS rule conflicts
+<div class="bg-gray-100 p-4">
+  <div class="p-2 bg-red-500">
+    This text now has the correct red background and padding.
+  </div>
+</div>
+```
